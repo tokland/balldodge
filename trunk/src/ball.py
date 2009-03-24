d3d08777f1f0c9bf067182a5f6229178813dfe09
tokland <tokland@ff7d764e-a12d-0410-b984-2f4dfc39e4c6>
2009-03-24 21:41:13 +0000

    commit first running version

--- trunk/src/ball.py
+++ trunk/src/ball.py
@@ -4,166 +4,166 @@ import math
 import gamelib
 
 def process_balls_collision((m1, v1), (m2, v2)):
-	a = (m1-m2)/(m1+m2)
-	b = 2*m2/(m1+m2)
-	new_v1 = a*v1+b*v2
-	new_v2 = (m1/m2)*b*v1-a*v2
-	return new_v1, new_v2
+    a = (m1-m2)/(m1+m2)
+    b = 2*m2/(m1+m2)
+    new_v1 = a*v1+b*v2
+    new_v2 = (m1/m2)*b*v1-a*v2
+    return new_v1, new_v2
 
 ##############################################
 class BallBase:
-	def __init__(self, radius, color, circle_npoints):
-		self.pos = (0, 0)
-		self.vpos = visual.vector(self.pos)
-		self.angle = 0.0
-		self.set_speed((0, 0))
-		self.circle_npoints = circle_npoints
-		self.set_radius(radius)
-		self.set_color(color)
-		self.update_base_circle_points()
-		
-	def get_radius(self):
-		return self.radius
-		
-	def update_base_circle_points(self):
-		pi2 = 2*math.pi
-		sin, cos = math.sin, math.cos
-		self.base_circle_points = [(self.radius*cos(angle), \
-			self.radius*sin(angle)) for angle in \
-			[(index*pi2)/self.circle_npoints for \
-			index in xrange(self.circle_npoints)]]
+    def __init__(self, radius, color, circle_npoints):
+        self.pos = (0, 0)
+        self.vpos = visual.vector(self.pos)
+        self.angle = 0.0
+        self.set_speed((0, 0))
+        self.circle_npoints = circle_npoints
+        self.set_radius(radius)
+        self.set_color(color)
+        self.update_base_circle_points()
+        
+    def get_radius(self):
+        return self.radius
+        
+    def update_base_circle_points(self):
+        pi2 = 2*math.pi
+        sin, cos = math.sin, math.cos
+        self.base_circle_points = [(self.radius*cos(angle), \
+            self.radius*sin(angle)) for angle in \
+            [(index*pi2)/self.circle_npoints for \
+            index in xrange(self.circle_npoints)]]
 
-	def get_bounds(self):
-		(x, y), r = self.pos, self.radius
-		return (x-r, y-r), (x+r, y+r)
-		
-	def set_pos(self, pos):
-		self.pos = pos
-		self.vpos = visual.vector(pos)
+    def get_bounds(self):
+        (x, y), r = self.pos, self.radius
+        return (x-r, y-r), (x+r, y+r)
+        
+    def set_pos(self, pos):
+        self.pos = pos
+        self.vpos = visual.vector(pos)
 
-	def set_vpos(self, vpos):
-		self.vpos = vpos
-		self.pos = vpos.astuple()[:2]
+    def set_vpos(self, vpos):
+        self.vpos = vpos
+        self.pos = vpos.astuple()[:2]
 
-	def get_pos(self):
-		return self.pos
+    def get_pos(self):
+        return self.pos
 
-	def update_circle_points(self):
-		self.circle_points = [[(a+b) for (a, b) in zip(self.pos, point)] \
-			for point in self.base_circle_points]
+    def update_circle_points(self):
+        self.circle_points = [[(a+b) for (a, b) in zip(self.pos, point)] \
+            for point in self.base_circle_points]
 
-	def set_color(self, color):
-		self.color = color
-		
-	def get_circle_points(self):
-		self.update_circle_points()
-		return self.circle_points
-		
-	def set_speed(self, speed):
-		self.speed = speed
-		self.vspeed = visual.vector(*speed)
+    def set_color(self, color):
+        self.color = color
+        
+    def get_circle_points(self):
+        self.update_circle_points()
+        return self.circle_points
+        
+    def set_speed(self, speed):
+        self.speed = speed
+        self.vspeed = visual.vector(*speed)
 
-	def set_vspeed(self, vspeed):
-		self.vspeed = visual.vector(*vspeed)
-		self.speed = vspeed.astuple()[:2]
+    def set_vspeed(self, vspeed):
+        self.vspeed = visual.vector(*vspeed)
+        self.speed = vspeed.astuple()[:2]
 
-	def set_radius(self, radius):
-		self.radius = float(radius)
-		self.update_base_circle_points()
-		self.update_circle_points()
+    def set_radius(self, radius):
+        self.radius = float(radius)
+        self.update_base_circle_points()
+        self.update_circle_points()
 
-	def collides_with_figures(self, figures):
-		bounds1 = self.get_bounds()
-		triangles = {}
-		for figure in figures:
-			if not gamelib.check_bounds(bounds1, figure.get_bounds()):
-				continue
-			circle_points = self.get_circle_points()
-			for point in circle_points:
-				for triangle in figure.get_triangles():
-					if triangle.collides_with_point(point):
-						if not triangles.has_key(triangle):
-							triangles[triangle] = []
-						triangles[triangle].append(point)
-		return [(triangle, triangles[triangle]) for triangle in triangles]
+    def collides_with_figures(self, figures):
+        bounds1 = self.get_bounds()
+        triangles = {}
+        for figure in figures:
+            if not gamelib.check_bounds(bounds1, figure.get_bounds()):
+                continue
+            circle_points = self.get_circle_points()
+            for point in circle_points:
+                for triangle in figure.get_triangles():
+                    if triangle.collides_with_point(point):
+                        if not triangles.has_key(triangle):
+                            triangles[triangle] = []
+                        triangles[triangle].append(point)
+        return [(triangle, triangles[triangle]) for triangle in triangles]
 
-	def update_collision_with_ball(self, ball, electron_mode=False):
-		point = self.collides_with_ball(ball)
-		if not electron_mode:
-			if not point or (self.vspeed.dot(point-self.vpos) <= 0 and \
-					ball.vspeed.dot(point-ball.vpos) <= 0):
-				return
-		elif not point or (self.vspeed.dot(point-self.vpos) > 0 or \
-			ball.vspeed.dot(point-ball.vpos) > 0):
-			return
-		vdiff = ball.vpos - self.vpos
-		vdiffo = vdiff.rotate(visual.pi/2.0)
-		v1c, v2c = [b.vspeed.proj(vdiff) for b in (self, ball)]
-		v1o, v2o = [b.vspeed.proj(vdiffo) for b in (self, ball)]
-		m1, m2 = self.radius**2, ball.radius**2
-		k1, k2 = [cmp(vdiff.dot(x), 0.0) for x in (v1c, v2c)]
-		nv1c, nv2c = process_balls_collision((m1, k1*abs(v1c)), \
-			(m2, k2*abs(v2c)))
-		self.vspeed = v1o + k1*v1c.norm()*nv1c
-		ball.vspeed = v2o + k2*v2c.norm()*nv2c
-		return point
-		
-	def collides_with_ball(self, ball):
-		vdiff = ball.vpos - self.vpos
-		if abs(vdiff) < self.radius+ball.radius:
-			nvdiff = vdiff.norm()
-			p1 = self.vpos+self.radius*nvdiff
-			p2 = ball.vpos-ball.radius*nvdiff
-			return (p1+p2)/2
+    def update_collision_with_ball(self, ball, electron_mode=False):
+        point = self.collides_with_ball(ball)
+        if not electron_mode:
+            if not point or (self.vspeed.dot(point-self.vpos) <= 0 and \
+                    ball.vspeed.dot(point-ball.vpos) <= 0):
+                return
+        elif not point or (self.vspeed.dot(point-self.vpos) > 0 or \
+            ball.vspeed.dot(point-ball.vpos) > 0):
+            return
+        vdiff = ball.vpos - self.vpos
+        vdiffo = vdiff.rotate(visual.pi/2.0)
+        v1c, v2c = [b.vspeed.proj(vdiff) for b in (self, ball)]
+        v1o, v2o = [b.vspeed.proj(vdiffo) for b in (self, ball)]
+        m1, m2 = self.radius**2, ball.radius**2
+        k1, k2 = [cmp(vdiff.dot(x), 0.0) for x in (v1c, v2c)]
+        nv1c, nv2c = process_balls_collision((m1, k1*abs(v1c)), \
+            (m2, k2*abs(v2c)))
+        self.vspeed = v1o + k1*v1c.norm()*nv1c
+        ball.vspeed = v2o + k2*v2c.norm()*nv2c
+        return point
+        
+    def collides_with_ball(self, ball):
+        vdiff = ball.vpos - self.vpos
+        if abs(vdiff) < self.radius+ball.radius:
+            nvdiff = vdiff.norm()
+            p1 = self.vpos+self.radius*nvdiff
+            p2 = ball.vpos-ball.radius*nvdiff
+            return (p1+p2)/2
 
-	def draw(self, surface, color=None, width=0):
-		if color is None:
-			color = self.color
-		return [pygame.draw.circle(surface, color, \
-			[int(x) for x in self.pos], int(self.radius), width)]
+    def draw(self, surface, color=None, width=0):
+        if color is None:
+            color = self.color
+        return [pygame.draw.circle(surface, color, \
+            [int(x) for x in self.pos], int(self.radius), width)]
 
 class Ball(BallBase):
-	def __init__(self, radius, color, solid, electron, circle_npoints=16):
-		BallBase.__init__(self, radius, color, circle_npoints)
-		self.solid = solid
-		self.electron = electron
-		self.debug_points = []
+    def __init__(self, radius, color, solid, electron, circle_npoints=16):
+        BallBase.__init__(self, radius, color, circle_npoints)
+        self.solid = solid
+        self.electron = electron
+        self.debug_points = []
 
-	def update_collision_with_ball(self, ball):
-		if self.solid and ball.solid:
-			electron = self.electron or ball.electron
-			return BallBase.update_collision_with_ball(self, ball, \
-				electron_mode=electron)
+    def update_collision_with_ball(self, ball):
+        if self.solid and ball.solid:
+            electron = self.electron or ball.electron
+            return BallBase.update_collision_with_ball(self, ball, \
+                electron_mode=electron)
 
-	def draw(self, surface):
-		rects = []
-		rects.extend(BallBase.draw(self, surface))
-		if self.solid:
-			color = (255, 255, 255)
-			rects.extend(BallBase.draw(self, surface, color=color, width=2))
-		rects.extend([pygame.draw.circle(surface, (255, 100, 100), \
-			[int(x) for x in point], 2) for point in self.debug_points])
-		return rects		
+    def draw(self, surface):
+        rects = []
+        rects.extend(BallBase.draw(self, surface))
+        if self.solid:
+            color = (255, 255, 255)
+            rects.extend(BallBase.draw(self, surface, color=color, width=2))
+        rects.extend([pygame.draw.circle(surface, (255, 100, 100), \
+            [int(x) for x in point], 2) for point in self.debug_points])
+        return rects		
 
-	def update(self, player, board, tick, check_pressed_key):
-		if check_pressed_key("space"):
-			tick = 0.0
-		self.set_vpos(self.vpos+tick*self.vspeed)		
-		self.debug_triangles = []
-		collision =  self.collides_with_figures(board.get_figures()+[player])
-		player_collide = False
-		if collision:
-			ptriangles = player.get_triangles()
-			for triangle, points in collision:
-				if triangle in ptriangles:
-					player_collide = True
-					break
-			triangle, points = collision[0]
-			vector = reduce(visual.vector.__add__, \
-				[(visual.vector(point)-self.vpos) for point in points])
-			if vector.dot(self.vspeed) >= 0:
-				orto = vector.rotate(visual.pi/2)
-				bfactor = board.get_rebound()
-				nvspeed = self.vspeed.proj(orto)-self.vspeed.proj(vector)
-				self.set_vspeed(bfactor*(nvspeed))
-		return player_collide
+    def update(self, player, board, tick, check_pressed_key):
+        if check_pressed_key("space"):
+            tick = 0.0
+        self.set_vpos(self.vpos+tick*self.vspeed)		
+        self.debug_triangles = []
+        collision =  self.collides_with_figures(board.get_figures()+[player])
+        player_collide = False
+        if collision:
+            ptriangles = player.get_triangles()
+            for triangle, points in collision:
+                if triangle in ptriangles:
+                    player_collide = True
+                    break
+            triangle, points = collision[0]
+            vector = reduce(visual.vector.__add__, \
+                [(visual.vector(point)-self.vpos) for point in points])
+            if vector.dot(self.vspeed) >= 0:
+                orto = vector.rotate(visual.pi/2)
+                bfactor = board.get_rebound()
+                nvspeed = self.vspeed.proj(orto)-self.vspeed.proj(vector)
+                self.set_vspeed(bfactor*(nvspeed))
+        return player_collide
--- trunk/src/objects.py
+++ trunk/src/objects.py
@@ -5,219 +5,219 @@ import visual
 import gamelib
 
 class Triangle:
-	def __init__(self, points, fsize=(1, 1), angle=0.0):
-		if len(points) != 3:
-			raise ValueError, "A triangle should have exactly 3 points"
-		self.angle = angle
-		self.vbasepoints = [visual.vector([(s*p) for (s, p) in zip(fsize, point)]) \
-			for point in points]
-		self.vpos = visual.vector((0, 0))
-		self.update_points()
-				
-	def set_vpos(self, pos):
-		self.vpos = pos
-		self.update_points()
-		
-	def get_pos(self):
-		return self.vpos.astuple()[:2]
-				
-	def set_angle(self, angle):
-		self.angle = angle
-		self.update_points()
-		
-	def update_points(self):
-		self.vpoints = [(v.rotate(self.angle)+self.vpos) \
-			for v in self.vbasepoints]
-		self.points = [v.astuple()[:2] for v in self.vpoints]
-		gp = lambda k: [p[k] for p in self.points]
-		px, py = gp(0), gp(1)
-		self.bounds = (min(px), min(py)), (max(px), max(py))
-			
-	def get_bounds(self):
-		return self.bounds
-		
-	def get_points(self):
-		return self.points
-		
-	def collides_with_point(self, point):
-		if not gamelib.check_bounds((point, point), self.get_bounds()):
-			return
-		p0, p1, p2 = self.points
-		orientation = self.get_orientation()
-		for triangle in (Triangle((q0, q1, q2)) for (q0, q1, q2) \
-				in ((p0, p1, point), (p1, p2, point), (p2, p0, point))):
-			if triangle.get_orientation()*orientation < 0:
-				return
-		return (p0, p1, p2)
+    def __init__(self, points, fsize=(1, 1), angle=0.0):
+        if len(points) != 3:
+            raise ValueError, "A triangle should have exactly 3 points"
+        self.angle = angle
+        self.vbasepoints = [visual.vector([(s*p) for (s, p) in zip(fsize, point)]) \
+            for point in points]
+        self.vpos = visual.vector((0, 0))
+        self.update_points()
+                
+    def set_vpos(self, pos):
+        self.vpos = pos
+        self.update_points()
+        
+    def get_pos(self):
+        return self.vpos.astuple()[:2]
+                
+    def set_angle(self, angle):
+        self.angle = angle
+        self.update_points()
+        
+    def update_points(self):
+        self.vpoints = [(v.rotate(self.angle)+self.vpos) \
+            for v in self.vbasepoints]
+        self.points = [v.astuple()[:2] for v in self.vpoints]
+        gp = lambda k: [p[k] for p in self.points]
+        px, py = gp(0), gp(1)
+        self.bounds = (min(px), min(py)), (max(px), max(py))
+            
+    def get_bounds(self):
+        return self.bounds
+        
+    def get_points(self):
+        return self.points
+        
+    def collides_with_point(self, point):
+        if not gamelib.check_bounds((point, point), self.get_bounds()):
+            return
+        p0, p1, p2 = self.points
+        orientation = self.get_orientation()
+        for triangle in (Triangle((q0, q1, q2)) for (q0, q1, q2) \
+                in ((p0, p1, point), (p1, p2, point), (p2, p0, point))):
+            if triangle.get_orientation()*orientation < 0:
+                return
+        return (p0, p1, p2)
 
-	def collides_with_points(self, points):
-		for point in points:
-			if self.collides_with_point(point):
-				return point
+    def collides_with_points(self, points):
+        for point in points:
+            if self.collides_with_point(point):
+                return point
 
-	def get_orientation(self):
-		p1, p2, p3 = self.vpoints
-		a, b = (p2-p1), (p3-p1)
-		return (a[0]*b[1]-a[1]*b[0])
+    def get_orientation(self):
+        p1, p2, p3 = self.vpoints
+        a, b = (p2-p1), (p3-p1)
+        return (a[0]*b[1]-a[1]*b[0])
 
-	def get_segments(self):
-		v1, v2, v3 = self.vpoints
-		return [Segment(start=vpoint.astuple()[:2], vector=vector) \
-			for vpoint, vector in [(v1, (v2-v1)), (v2, (v3-v2)), (v3, (v1-v3))]]
+    def get_segments(self):
+        v1, v2, v3 = self.vpoints
+        return [Segment(start=vpoint.astuple()[:2], vector=vector) \
+            for vpoint, vector in [(v1, (v2-v1)), (v2, (v3-v2)), (v3, (v1-v3))]]
 
-	def get_points_depth(self, depth):
-		output = self.points[:]
-		output.extend([v.astuple()[:2] for v in \
-			self.get_center_vpoints(self.vpoints, depth)])
-		return output
-	
-	def get_center_vpoints(self, vpoints, depth):
-		p1, p2, p3 = vpoints
-		p4 = (p1+p2+p3)/3
-		output = [p4]
-		if not depth:
-			return output
-		depth -= 1
-		output.extend([p for vps in [(p1, p2, p4), (p1, p3, p4), (p2, p3, p4)] \
-			for p in self.get_center_vpoints(vps, depth)])
-		return output
+    def get_points_depth(self, depth):
+        output = self.points[:]
+        output.extend([v.astuple()[:2] for v in \
+            self.get_center_vpoints(self.vpoints, depth)])
+        return output
+    
+    def get_center_vpoints(self, vpoints, depth):
+        p1, p2, p3 = vpoints
+        p4 = (p1+p2+p3)/3
+        output = [p4]
+        if not depth:
+            return output
+        depth -= 1
+        output.extend([p for vps in [(p1, p2, p4), (p1, p3, p4), (p2, p3, p4)] \
+            for p in self.get_center_vpoints(vps, depth)])
+        return output
 
 #t = Triangle([(-5, 4), (-4, 5), (-3, 4)])
-		
+        
 ##############################################
 class TrianglesFigure:
-	def __init__(self, triangles, fsize=None):
-		self.angle = 0.0
-		self.points = None
-		self.bounds = None
-		self.triangles = [Triangle(vertices, fsize) for vertices in triangles]
-		self.set_pos((0, 0))
-		self.f = False
+    def __init__(self, triangles, fsize=None):
+        self.angle = 0.0
+        self.points = None
+        self.bounds = None
+        self.triangles = [Triangle(vertices, fsize) for vertices in triangles]
+        self.set_pos((0, 0))
+        self.f = False
 
-	def draw(self, surface, color=(0, 0, 255), width=0):
-		rects = [pygame.draw.polygon(surface, color, \
-			t.get_points(), width) for t in self.triangles]
-		return rects
-		
-	def get_points(self):
-		self.update_points()
-		return self.points
-							
-	def get_bounds(self):
-		return self.bounds
+    def draw(self, surface, color=(0, 0, 255), width=0):
+        rects = [pygame.draw.polygon(surface, color, \
+            t.get_points(), width) for t in self.triangles]
+        return rects
+        
+    def get_points(self):
+        self.update_points()
+        return self.points
+                            
+    def get_bounds(self):
+        return self.bounds
 
-	def update_points(self):
-		points = [p for t in self.triangles for p in t.get_points()]
-		if not points:
-			return
-		px, py = [[p[k] for p in points] for k in range(2)]		
-		self.points = points
-		self.bounds = (min(px), min(py)), (max(px), max(py))
-		
-	def set_pos(self, pos):
-		vpos = visual.vector(pos)
-		for triangle in self.triangles:
-			triangle.set_vpos(vpos)
-		self.vpos = vpos
-		self.update_points()
-	
-	def add_vpos(self, incpos):
-		self.vpos += incpos
-		self.set_pos(self.vpos)
-		
-	def get_pos(self):
-		return self.vpos.astuple()[:2]
-		
-	def get_triangles(self):
-		return self.triangles
-		
-	def set_angle(self, angle):
-		self.angle = angle
-		for t in self.triangles:
-			t.set_angle(angle)
-		self.update_points()
+    def update_points(self):
+        points = [p for t in self.triangles for p in t.get_points()]
+        if not points:
+            return
+        px, py = [[p[k] for p in points] for k in range(2)]		
+        self.points = points
+        self.bounds = (min(px), min(py)), (max(px), max(py))
+        
+    def set_pos(self, pos):
+        vpos = visual.vector(pos)
+        for triangle in self.triangles:
+            triangle.set_vpos(vpos)
+        self.vpos = vpos
+        self.update_points()
+    
+    def add_vpos(self, incpos):
+        self.vpos += incpos
+        self.set_pos(self.vpos)
+        
+    def get_pos(self):
+        return self.vpos.astuple()[:2]
+        
+    def get_triangles(self):
+        return self.triangles
+        
+    def set_angle(self, angle):
+        self.angle = angle
+        for t in self.triangles:
+            t.set_angle(angle)
+        self.update_points()
 
-	def add_angle(self, increment):
-		self.angle += increment
-		for t in self.triangles:
-			t.set_angle(self.angle)
-		self.update_points()
-		
-	def get_angle(self):
-		return self.angle
-					
-	def collides_with_figures(self, figures):
-		bounds1 = self.get_bounds()
-		for figure in figures:
-			fbounds = figure.get_bounds()
-			if not gamelib.check_bounds(bounds1, fbounds):
-				continue
-			for t1 in self.get_triangles():
-				if not gamelib.check_bounds(bounds1, t1.get_bounds()):
-					continue
-				t1segs = t1.get_segments()
-				for t2 in figure.get_triangles():
-					for seg1 in t1segs:
-						for seg2 in t2.get_segments():
-							if seg1.collides_with_segment(seg2):
-								return (t1, t2)
+    def add_angle(self, increment):
+        self.angle += increment
+        for t in self.triangles:
+            t.set_angle(self.angle)
+        self.update_points()
+        
+    def get_angle(self):
+        return self.angle
+                    
+    def collides_with_figures(self, figures):
+        bounds1 = self.get_bounds()
+        for figure in figures:
+            fbounds = figure.get_bounds()
+            if not gamelib.check_bounds(bounds1, fbounds):
+                continue
+            for t1 in self.get_triangles():
+                if not gamelib.check_bounds(bounds1, t1.get_bounds()):
+                    continue
+                t1segs = t1.get_segments()
+                for t2 in figure.get_triangles():
+                    for seg1 in t1segs:
+                        for seg2 in t2.get_segments():
+                            if seg1.collides_with_segment(seg2):
+                                return (t1, t2)
 
-	def collides_with_point(self, pos):
-		if not gamelib.check_bounds(self.get_bounds(), (pos, pos)):
-			return
-		for triangle in self.triangles:
-			if not gamelib.check_bounds(triangle.get_bounds(), (pos, pos)):
-				continue
-			if triangle.collides_with_point(pos):
-				return triangle
+    def collides_with_point(self, pos):
+        if not gamelib.check_bounds(self.get_bounds(), (pos, pos)):
+            return
+        for triangle in self.triangles:
+            if not gamelib.check_bounds(triangle.get_bounds(), (pos, pos)):
+                continue
+            if triangle.collides_with_point(pos):
+                return triangle
 
 class Segment:
-	def __init__(self, start, end=None, vector=None):
-		self.p1 = start
-		if end is not None:
-			self.p2 = end
-			self.vector = visual.vector(end)-visual.vector(start)
-		elif vector is not None:
-			self.vector = vector
-			self.p2 = (visual.vector(start)+vector).astuple()[:2]
-		self.update_bounds()
-	
-	def __repr__(self):
-		return "<Segment: %s -> %s>"%(self.p1, self.p2)
-		
-	def get_bounds(self):
-		return self.bounds
-	
-	def update_bounds(self):
-		(x1, y1), (x2, y2) = self.p1, self.p2
-		self.bounds = [(min(a), max(a)) for a in ((x1, x2), (y1, y2))]
-		
-	def get_points(self):
-		return (self.p1, self.p2)
-		
-	def get_vector(self):
-		return self.vector
+    def __init__(self, start, end=None, vector=None):
+        self.p1 = start
+        if end is not None:
+            self.p2 = end
+            self.vector = visual.vector(end)-visual.vector(start)
+        elif vector is not None:
+            self.vector = vector
+            self.p2 = (visual.vector(start)+vector).astuple()[:2]
+        self.update_bounds()
+    
+    def __repr__(self):
+        return "<Segment: %s -> %s>"%(self.p1, self.p2)
+        
+    def get_bounds(self):
+        return self.bounds
+    
+    def update_bounds(self):
+        (x1, y1), (x2, y2) = self.p1, self.p2
+        self.bounds = [(min(a), max(a)) for a in ((x1, x2), (y1, y2))]
+        
+    def get_points(self):
+        return (self.p1, self.p2)
+        
+    def get_vector(self):
+        return self.vector
 
-	def isbetween(self, n, a, b, epsilon=0.000001):
-		if a > b:
-			a, b = b, a
-		return (n-a)>-epsilon and (b-n)>-epsilon
+    def isbetween(self, n, a, b, epsilon=0.000001):
+        if a > b:
+            a, b = b, a
+        return (n-a)>-epsilon and (b-n)>-epsilon
 
-	def collides_with_segment(self, segment):
-		p0x, p0y = self.p1
-		t0x, t0y = segment.p1
-		vx, vy = self.vector.astuple()[:2]
-		wx, wy = segment.vector.astuple()[:2]
-		m = numpy.matrix([(vx, -wx), (vy, -wy)])
-		r = numpy.matrix([[t0x-p0x], [t0y-p0y]])
-		try: 
-			k1 = float((numpy.linalg.inv(m)*r)[0])
-		except numpy.linalg.linalg.LinAlgError: 
-			return
-		xc, yc = (p0x+k1*vx, p0y+k1*vy)
-		(x1, y1), (x2, y2) = self.p1, self.p2
-		(lx1, ly1), (lx2, ly2) = segment.p1, segment.p2
-		ib = self.isbetween
-		if ib(xc, x1, x2) and ib(xc, lx1, lx2) \
-				and ib(yc, y1, y2) and ib(yc, ly1, ly2):
-			return xc, yc
+    def collides_with_segment(self, segment):
+        p0x, p0y = self.p1
+        t0x, t0y = segment.p1
+        vx, vy = self.vector.astuple()[:2]
+        wx, wy = segment.vector.astuple()[:2]
+        m = numpy.matrix([(vx, -wx), (vy, -wy)])
+        r = numpy.matrix([[t0x-p0x], [t0y-p0y]])
+        try: 
+            k1 = float((numpy.linalg.inv(m)*r)[0])
+        except numpy.linalg.linalg.LinAlgError: 
+            return
+        xc, yc = (p0x+k1*vx, p0y+k1*vy)
+        (x1, y1), (x2, y2) = self.p1, self.p2
+        (lx1, ly1), (lx2, ly2) = segment.p1, segment.p2
+        ib = self.isbetween
+        if ib(xc, x1, x2) and ib(xc, lx1, lx2) \
+                and ib(yc, y1, y2) and ib(yc, ly1, ly2):
+            return xc, yc
